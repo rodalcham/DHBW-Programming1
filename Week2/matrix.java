f74f7535934a6f0afacd87eca77c40deb0742185
rodalcham <rodalcham@gmail.com>
2026-01-16 15:15:31 +0100

    Second week exercise

---
+++ Week2/matrix.java
@@ -0,0 +1,29 @@
+public class MatrixElementWise {
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] B = {
+            {7, 8, 9},
+            {10, 11, 12}
+        };
+
+        int rows = A.length;
+        int cols = A[0].length;
+        int[][] C = new int[rows][cols];
+
+        // Element-wise addition
+        for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				C[i][j] = A[i][j] + B[i][j];
+			}
+		}
+
+        // Print result matrix
+        TODO
+    }
+}
